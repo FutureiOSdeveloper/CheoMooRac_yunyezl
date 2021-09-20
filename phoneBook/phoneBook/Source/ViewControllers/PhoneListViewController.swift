3313b0ef4e49bfb90c41b1923edc7431921ae36f
yejiyun <20172062@sungshin.ac.kr>
2021-09-21 01:54:01 +0900

    [#1] 기초 셋팅
    ### Description
    -  SnapKit, Then, SwiftLint 라이브러리 설치
    -  폴더링
    
    ### Related Issue
    Resolve #1

---
+++ phoneBook/phoneBook/Source/ViewControllers/PhoneListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PhoneListViewController.swift
+//  phoneBook
+//
+//  Created by 윤예지 on 2021/09/21.
+//
+
+import UIKit
+import SnapKit
+
+class PhoneListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
