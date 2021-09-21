be2310ca3d4a735a29bf5cc443170017eaf1d6e0
yejiyun <20172062@sungshin.ac.kr>
2021-09-21 18:23:11 +0900

    [#1] navigation Controller 설정

--- phoneBook/phoneBook/Source/ViewControllers/PhoneListViewController.swift
+++ phoneBook/phoneBook/Source/ViewControllers/PhoneListViewController.swift
@@ -13,6 +13,22 @@ class PhoneListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        initSearchNavigationController()
+        initNavigationBarButtonItem()
     }
-
-}
+    private func initSearchNavigationController() {
+        searchController.searchBar.placeholder = "검색"
+        navigationItem.title = "연락처"
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    private func initNavigationBarButtonItem() {
+        let groupBarButtonItem = UIBarButtonItem(title: "그룹", style: .plain, target: self, action: nil)
+        let plusBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: nil)
+        
+        navigationItem.leftBarButtonItem = groupBarButtonItem
+        navigationItem.rightBarButtonItem = plusBarButtonItem
+    }
+    
